7172da26e6497a93a141a414395e5fdb0a8a991e
Mananshi <66679952+Mananshi@users.noreply.github.com>
2022-11-04 21:41:15 +0530

    Add: Middleware

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-import express from "express"
+import express  from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 
-const app = express()
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World")
-    }
-)   
+app.use(cors());
 
-app.listen(3000, () => {
-    console.log("Server is running")
-})
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/details', (req, res) => {
+    res.send({data: 'Hello World, from express'});
+});
+
+app.listen(port, () => console.log(`Hello world app listening on http://localhost:${port}`))
