90b4b84f013080d7d43da408e0bac598cac76bfe
Mananshi <66679952+Mananshi@users.noreply.github.com>
2022-11-06 18:19:06 +0530

    Add: email and password validation

--- models/User.js
+++ models/User.js
@@ -47,11 +47,11 @@ User.loginUser = async (req, res) => {
                     res.status(200).json({token})
                 }
                 else{
-                    res.status(401).json({message: 'Wrong password'})
+                    res.status(401).json({message: 'Incorrect password'})
                 }
             }
             else {
-                res.status(401).json({message: 'Wrong email'})
+                res.status(401).json({message: 'User doesn\'t exist'})
             }
         } else {
             res.status(401).json({message: 'Invalid email'})
@@ -65,15 +65,25 @@ User.loginUser = async (req, res) => {
 User.signupUser = async (req, res) => {
     try{
         const {name, email, password} = req.body
-        if(email){
+        const validEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+        const validPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/
+        if(validEmail.test(email)){
             const user = await User.findOne({where: {email: email}});
             if(user){
                 res.status(401).json({message: 'Account already exists'})
             }
             else {
-                await User.create({name, email, password})
-                res.status(200).json({message: 'Account created'})
-            }
+                if(validPassword.test(password)){
+                    await User.create({name, email, password})
+                    res.status(200).json({message: 'Account created'})
+                }
+                else {
+                    res.status(401).json({message: 'Password must contain at least 6 characters, one uppercase, one lowercase, one number and one special character'})
+                }
+                }
+        }
+        else{
+            res.status(401).json({message: 'Please enter a valid email'})
         }
     }
     catch(err){
